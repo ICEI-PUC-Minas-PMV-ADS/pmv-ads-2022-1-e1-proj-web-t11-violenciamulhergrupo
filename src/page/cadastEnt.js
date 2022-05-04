0e43be388f010389410183ae446dc63522122c58
EdgleiMarques <edgleitab@gmail.com>
2022-05-03 23:44:43 -0300

    Implamentação do cadastro em JSon

--- src/page/cadastEnt.js
+++ src/page/cadastEnt.js
@@ -1,5 +1,17 @@
-let nome = document.getElementById("name");
-let teste = "Cadastrado com sucesso"
 function salva(){
-    alert(teste)
+    let nome = document.querySelector("#name");
+    
+
+
+    let cadastro = {
+        nome:"",
+        endereço:"",
+        telefone:0,
+        url:""
+
+    }
+    cadastro.nome = nome.value;
+    
+    let cadastroJson =JSON.stringify(cadastro);
+    alert("Cadastro da Institutição\n"+cadastro.nome+"\nRealizado com sucesso!!\nPara alterar alguma informação acesse nosso site");
 }
