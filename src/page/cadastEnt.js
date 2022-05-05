b07af9ceb5e5542a8fd974bd44e29fa538d5e1a9
EdgleiMarques <edgleitab@gmail.com>
2022-05-04 23:01:45 -0300

    Conclusão da da captura dos dados da Instituição

--- src/page/cadastEnt.js
+++ src/page/cadastEnt.js
@@ -1,17 +1,24 @@
+function naoMudar(e){
+    e.preventDefault();
+}
 function salva(){
-    let nome = document.querySelector("#name");
+    const nome = document.getElementById("name");
+    const fone = document.getElementById("phone");
+    const endereco = document.getElementById("adress");
+    const url = document.getElementById("url");
+    const cidade = document.getElementById("city")
+    const estado = document.getElementById("state")
+    const descricao = document.getElementById("description");
     
 
-
-    let cadastro = {
-        nome:"",
-        endereço:"",
-        telefone:0,
-        url:""
-
-    }
+    let cadastro = {}
     cadastro.nome = nome.value;
-    
+    cadastro.fone = parseInt(fone.value);
+    cadastro.endereco = endereco.value;
+    cadastro.url = url.value;
+    cadastro.cidade = cidade.value;
+    cadastro.estado = estado.value;
+    cadastro.descricao = descricao.value;
     let cadastroJson =JSON.stringify(cadastro);
     alert("Cadastro da Institutição\n"+cadastro.nome+"\nRealizado com sucesso!!\nPara alterar alguma informação acesse nosso site");
 }
