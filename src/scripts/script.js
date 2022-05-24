50625078a98fcc7acd283ada9727607f7d1836d6
Pedro Victor <pedro20002victor@gmail.com>
2022-05-24 06:00:36 -0300

    Update Header

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,7 +1,7 @@
 let btnMenuLateral = document.getElementById("menu");
 let menuLateral = document.querySelector(".nav-bottom");
 
-
+console.log("oi")
 
 
 // Elements Colors
@@ -22,24 +22,24 @@ btnColorMode.addEventListener("click", ()=>{
 
 
 
-
-
-
-
 btnMenuLateral.addEventListener("click", () => {
+   console.log("oiii")
    console.log(menuLateral.className)
    menuLateral.classList.toggle("nav-bottom-active");
 })
+
+
+
 // Adiciona instituições cadastradas no Card
-const instName = document.getElementById("cardname")
-const instUrl = document.getElementById("insturl")
-const instImagem = document.getElementById("instimage")
-const instDescr = document.getElementById("instdescr")
-let cadastro = localStorage.getItem("cadastro")
-const objCadastro = JSON.parse(cadastro)
-console.log(objCadastro)
-instName.innerText = `Nome: ${objCadastro.nome}`
-instUrl.innerText = `URL: ${objCadastro.url}`
-//objCadastro.image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKagCUurnUxAeJnxXXsZVY329SkiIsiGqa_Q&usqp=CAU"
-instImagem.src = objCadastro.image
-instDescr.innerText = objCadastro.descricao;
+// const instName = document.getElementById("cardname")
+// const instUrl = document.getElementById("insturl")
+// const instImagem = document.getElementById("instimage")
+// const instDescr = document.getElementById("instdescr")
+// let cadastro = localStorage.getItem("cadastro")
+// const objCadastro = JSON.parse(cadastro)
+// console.log(objCadastro)
+// instName.innerText = `Nome: ${objCadastro.nome}`
+// instUrl.innerText = `URL: ${objCadastro.url}`
+// //objCadastro.image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKagCUurnUxAeJnxXXsZVY329SkiIsiGqa_Q&usqp=CAU"
+// instImagem.src = objCadastro.image
+// instDescr.innerText = objCadastro.descricao;
