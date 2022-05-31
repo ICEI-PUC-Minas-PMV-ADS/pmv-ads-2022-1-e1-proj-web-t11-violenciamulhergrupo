2ac47f0d144ab8c2188a42fc73714cf4f1d93106
Stephanie Ingrid <stephanieingrid5@gmail.com>
2022-05-30 22:42:58 -0300

    adicionado página de fórum

---
+++ src/scripts/scriptForum.js
@@ -0,0 +1,10 @@
+let campo1 = document.getElementById("mens1")
+let campo2 = document.getElementById("mens2")
+let campo3 = document.getElementById("mens3")
+let campo4 = document.getElementById("mens4")
+ 
+function GravarMens(){
+    alert("testando")
+   let texto = document.getElementById("texto")
+   campo1.innerText = texto.value 
+}
